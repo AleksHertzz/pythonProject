cc3ff884f7696e41c1b639c9a5350c2d6c01c734
ALeksHertz <sashahertz@bk.ru>
2021-11-03 17:15:35 +0500

    new

--- EdPlatform/EdPlatform/settings.py
+++ EdPlatform/EdPlatform/settings.py
@@ -75,16 +75,12 @@ WSGI_APPLICATION = 'EdPlatform.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'mssql',
-        'NAME': 'Education',
-        'USER': 'project_',
-        'PASSWORD': 'qwerty',
-        'HOST': 'LAPTOP-GAPQ08HB\SQLEXPRESS',
-        'PORT': '',
-        'OPTIONS': {"driver": "ODBC Driver 17 for SQL Server",
-        },
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+
 }
 
 
